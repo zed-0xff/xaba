4a83cd042fadd21efab513492db02b43804755ec
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2024-01-20 08:42:30 +0200

    initial

---
+++ spec/xaba_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Xaba do
+  it "has a version number" do
+    expect(Xaba::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
